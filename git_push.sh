54e923bb0043899f5a0e770233be7fc815fad397
madhavasthana-oss <madhavasthana@gmail.com>
2026-02-08 12:25:31 +0530

    instructions to push + imports

---
+++ git_push.sh
@@ -0,0 +1,3 @@
+git add <path_to_file_to_be_pushed or .>
+git commit -m "<information about commit, what you are adding>"
+git push
---
+++ imports.py
@@ -0,0 +1,6 @@
+import torch
+import torchvision
+import sklearn
+import skimage
+import wandb
+from torch.utils.data import Dataset, DataLoader
