c42b2b6251f6934d4c628ff298a0a25a3e070148
Ashwathpat <ashwathpatil94@gmail.com>
2026-02-08 16:10:08 +0530

    added freeze_backbone

--- SLD.py
+++ SLD.py
@@ -3,23 +3,29 @@ from imports import *
 class SLD(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        
-        self.backbone = models.resnet50(pretrained=True)
-        
-        self.backbone = nn.Sequential(*list(self.backbone.children())[:-1])
-        
-        in_features = 2048  # ResNet-50 final layer feature size
-        
-        self.header = nn.Sequential(
+
+        backbone = models.resnet50(weights="DEFAULT")
+
+        # remove FC layer
+        self.backbone = nn.Sequential(*list(backbone.children())[:-1])
+
+        in_features = 2048
+
+        # architecture design
+        self.head = nn.Sequential(
             nn.Flatten(),
-            nn.layerNorm(in_features),
+            nn.LayerNorm(in_features),
             nn.Linear(in_features, 512),
             nn.GELU(),
             nn.Dropout(0.3),
             nn.Linear(512, num_classes)
         )
-        
+
     def forward(self, x):
-        features = self.backbone(x)
-        out = self.header(features)
-        return out
+        x = self.backbone(x)
+        x = self.head(x)
+        return x
+
+    def freeze_backbone(self):
+    for p in self.backbone.parameters():
+        p.requires_grad = False
