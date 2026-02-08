97bb8001c46ece4afa190cb8140dd3802d231d9b
madhavasthana-oss <madhavasthana@gmail.com>
2026-02-08 15:53:20 +0530

    handled errors in SLD

--- SLD.py
+++ SLD.py
@@ -4,15 +4,15 @@ class SLD(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         
-        self.model = models.resnet50(pretrained=True)
+        self.backbone = models.resnet50(pretrained=True)
         
-        self.backbone = nn.Sequential(*list(backbone.children())[:-1])
+        self.backbone = nn.Sequential(*list(self.backbone.children())[:-1])
         
         in_features = 2048  # ResNet-50 final layer feature size
         
         self.header = nn.Sequential(
             nn.Flatten(),
-            nn.layerNorm(in_features)
+            nn.layerNorm(in_features),
             nn.Linear(in_features, 512),
             nn.GELU(),
             nn.Dropout(0.3),
